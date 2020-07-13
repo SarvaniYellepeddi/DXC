410f8869e13f88966a8f2090f09f1be252f7f6f1
SarvaniYellepeddi <1210716150@gitam.in>
2020-07-13 20:16:01 +0530

    initial commit

---
+++ Program 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace Program_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double fahrenheit;
+
+			double celsius = 36;
+			Console.WriteLine("Celsius: " + celsius);
+
+			fahrenheit = (celsius * 9) / 5 + 32;
+			Console.WriteLine("Fahrenheit: " + fahrenheit);
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Program3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+            int a, b, c;
+            Console.Write("Enter the value of A:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the value of B:");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the value of C:");
+            c = Convert.ToInt32(Console.ReadLine());
+            if (a > b && a > c)
+                Console.WriteLine("A is Largest");
+            else if (b > a && b > c)
+                Console.WriteLine("B is Largest");
+            else if (c > a && c > b)
+                Console.WriteLine("C is Largest");
+            else
+                Console.WriteLine("equals");
+
+            Console.ReadKey();
+        }
+	}
+}
